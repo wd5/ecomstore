25f75d66b8f4baf193707ecb3aeb6d894a833b7f
Vladimir PAL <freebsdstuff@MacBook-Vladimir-PAL.local>
2011-01-17 23:39:57 +0300

    Все сделал, как в книге написано

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from ecomstore.catalog.models import Category, Product
 from django.template import RequestContext
+from cart import cart
 
 def index(request, template_name="catalog/index.html"):
     page_title = 'Musical Instruments and Sheet Music for Musicians'
